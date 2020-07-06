94df3ccf3788dc72d54d382c89128646f4d1a43a
Guillermo Carrillo <gach87@gmail.com>
2020-07-05 23:32:23 -0400

    Initial commit

---
+++ src/app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <ConduitHeader/>
+    <router-view />
+    <ConduitFooter/>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import ConduitHeader from "@/header";
+import ConduitFooter from "@/footer";
+export default {
+  name: "App",
+  components: {
+    ConduitHeader,
+    ConduitFooter
+  }
+};
+</script>
