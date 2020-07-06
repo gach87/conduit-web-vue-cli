7913009d1e345ad564617b5076445bc1cf6fc4cf
Guillermo Carrillo <gach87@gmail.com>
2020-07-06 00:24:19 -0400

    Wired article feed component

---
+++ src/articles-feed.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="feed-toggle">
+    <ul class="nav nav-pills outline-active">
+      <li v-for="feed in feeds" :key="feed.id" class="nav-item">
+        <a
+          class="nav-link"
+          v-bind:class="{'active': feed.selected}"
+          v-on:click="select(feed)"
+        >{{feed.name}}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ArticlesFeed",
+  props: ["feeds"],
+  methods: {
+    select(feed) {
+      return this.$emit("feed-selected", feed);
+    }
+  }
+};
+</script>
--- src/home-page.vue
+++ src/home-page.vue
@@ -9,35 +9,7 @@
     <div class="container page">
       <div class="row">
         <div class="col-md-9">
-          <!-- <div class="feed-toggle">
-            <ul class="nav nav-pills outline-active">
-              <li v-if="isAuthenticated" class="nav-item">
-                <router-link
-                  :to="{ name: 'home-my-feed' }"
-                  class="nav-link"
-                  active-class="active"
-                >Your Feed</router-link>
-              </li>
-              <li class="nav-item">
-                <router-link
-                  :to="{ name: 'home' }"
-                  exact
-                  class="nav-link"
-                  active-class="active"
-                >Global Feed</router-link>
-              </li>
-              <li class="nav-item" v-if="tag">
-                <router-link
-                  :to="{ name: 'home-tag', params: { tag } }"
-                  class="nav-link"
-                  active-class="active"
-                >
-                  <i class="ion-pound"></i>
-                  {{ tag }}
-                </router-link>
-              </li>
-            </ul>
-          </div>-->
+          <ArticlesFeed v-bind:feeds="feeds" v-on:feed-selected="onFeedSelected($event)"></ArticlesFeed>
         </div>
         <div class="col-md-3">
           <PopularTags v-bind:tags="tags" v-on:tag-selected="onTagSelected($event)"></PopularTags>
@@ -49,19 +21,41 @@
 
 <script>
 import PopularTags from "./popular-tags";
+import ArticlesFeed from "./articles-feed";
 import HomePageService from "./home-page-service";
+
 export default {
   name: "home",
-  components: { PopularTags },
+  components: { PopularTags, ArticlesFeed },
   data() {
-    return { tags: undefined };
+    return {
+      tags: undefined,
+      feeds: [
+        { id: "personal", name: "Your feed", selected: false, isTag: false },
+        { id: "all", name: "Global Feed", selected: true, isTag: false }
+      ]
+    };
   },
   created() {
     HomePageService.fetchTags().then(tags => (this.tags = tags));
   },
   methods: {
     onTagSelected(tag) {
-      console.log(tag);
+      const tagFeed = {
+        id: tag.toLowerCase(),
+        name: "#" + tag,
+        selected: true,
+        isTag: true
+      };
+
+      this.feeds = this.feeds
+        .map(feed => Object.assign(feed, { selected: false }))
+        .concat([tagFeed]);
+    },
+    onFeedSelected(selectedFeed) {
+      this.feeds = this.feeds.map(feed =>
+        Object.assign(feed, { selected: feed.id === selectedFeed.id })
+      );
     }
   }
 };
