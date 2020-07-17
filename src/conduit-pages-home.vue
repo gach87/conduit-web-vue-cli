4241e03267432983fa4c94bbb8e3567170750f25
Guillermo Carrillo <gach87@gmail.com>
2020-07-17 00:22:09 -0400

    Simplified design

--- src/conduit-pages-home.vue
+++ src/conduit-pages-home.vue
@@ -10,12 +10,12 @@
       <div class="row">
         <div class="col-md-9">
           <ConduitArticlesFeed
-            v-bind:feeds="feeds"
-            v-bind:selected="selectedFeed"
+            v-bind:feeds="state.feeds"
+            v-bind:selected="state.selectedFeed"
             v-on:select="onFeedSelected($event)"
           ></ConduitArticlesFeed>
 
-          <div v-for="article in articles" :key="article.slug" v-bind:article="article">
+          <div v-for="article in state.articles" :key="article.slug" v-bind:article="article">
             <ConduitArticlePreview v-bind:article="article">
               <ConduitArticleMeta v-bind:article="article">
                 <ConduitButtonsFavorite
@@ -27,13 +27,13 @@
           </div>
 
           <ConduitLayoutPagination
-            v-bind:pages="pages"
-            v-bind:selected="selectedPage"
+            v-bind:pages="state.pages"
+            v-bind:selected="state.selectedPage"
             v-bind:onSelect="onPageSelected"
           ></ConduitLayoutPagination>
         </div>
         <div class="col-md-3">
-          <ConduitTagsPopular v-bind:tags="tags" v-on:select="onTagSelected($event)"></ConduitTagsPopular>
+          <ConduitTagsPopular v-bind:tags="state.tags" v-on:select="onTagSelected($event)"></ConduitTagsPopular>
         </div>
       </div>
     </div>
@@ -61,59 +61,42 @@ export default {
   },
   data() {
     return {
-      tags: undefined,
-      feeds: undefined,
-      selectedFeed: undefined,
-      articles: undefined,
-      selectedPage: undefined,
-      pages: undefined
+      state: {
+        tags: undefined,
+        feeds: undefined,
+        selectedFeed: undefined,
+        articles: undefined,
+        selectedPage: undefined,
+        pages: undefined
+      }
     };
   },
   created() {
-    ConduitHomePageService.init().then(state => this.setState(state));
+    ConduitHomePageService.init().then(
+      state => (this.state = Object.assign({}, state))
+    );
   },
   methods: {
     onTagSelected(tag) {
       ConduitHomePageService.onTagSelected({
         tag,
-        state: this.getState()
-      }).then(state => this.setState(state));
+        state: Object.assign({}, this.state)
+      }).then(state => (this.state = Object.assign({}, state)));
     },
     onFeedSelected(feed) {
       ConduitHomePageService.onFeedSelected({
         feed,
-        state: this.getState()
-      }).then(state => this.setState(state));
+        state: Object.assign({}, this.state)
+      }).then(state => (this.state = Object.assign({}, state)));
     },
     onPageSelected(page) {
       ConduitHomePageService.onPageSelected({
         page,
-        state: this.getState()
-      }).then(state => this.setState(state));
+        state: Object.assign({}, this.state)
+      }).then(state => (this.state = Object.assign({}, state)));
     },
-
     onFavoritedArticle(article) {
       console.log(article);
-    },
-    getState() {
-      return JSON.parse(
-        JSON.stringify({
-          articles: this.articles,
-          pages: this.pages,
-          tags: this.tags,
-          feeds: this.feeds,
-          selectedFeed: this.selectedFeed,
-          selectedPage: this.selectedPage
-        })
-      );
-    },
-    setState(input) {
-      this.articles = input.articles;
-      this.pages = input.pages;
-      this.tags = input.tags;
-      this.feeds = input.feeds;
-      this.selectedFeed = input.selectedFeed;
-      this.selectedPage = input.selectedPage;
     }
   }
 };
